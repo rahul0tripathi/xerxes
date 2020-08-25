de3df00f6667c7ec204937c8205ddf051938368c
Rahul Tripathi <rahultripathidev@gmail.com>
2020-08-25 17:02:44 +0530

    added formatting

--- kong/kongapi.go
+++ kong/kongapi.go
@@ -6,20 +6,22 @@ import (
 	"errors"
 	"fmt"
 	"github.com/rahultripathidev/docker-utility/config"
+	"io/ioutil"
 	"net/http"
 )
 var (
 	basePath string
 )
 func init(){
-	config.LoadServices("$HOME/.orchestrator/configuration")
-	err := config.LoadKongConf("$HOME/.orchestrator/configuration")
+	config.LoadServices(config.ConfigDir)
+	err := config.LoadKongConf(config.ConfigDir)
 	if err != nil {
 		panic(err)
 	}
 	basePath = config.KongConf.Host +":"+config.KongConf.Admin
 }
 func CreateNewUpstream(service config.KongDesc) error {
+	fmt.Print("\nCreating a new UpStream [",service.UpstreamService,"] .... ")
 	_body , _:= json.Marshal(Upstream{
 		service.UpstreamService,
 		"ip",
@@ -30,6 +32,7 @@ func CreateNewUpstream(service config.KongDesc) error {
 		return err
 	}
 	if res.StatusCode == 200 || res.StatusCode == 409{
+		fmt.Println("OK")
 		return nil
 	}else{
 		return errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
@@ -37,18 +40,18 @@ func CreateNewUpstream(service config.KongDesc) error {
 	}
 }
 func AddUpstreamTarget(service config.KongDesc , target UpstreamTarget) error {
+	fmt.Print("\nAdding Upstream Target [",target.Target,"] to [",service.UpstreamService,"] .... ")
 	_body , err := json.Marshal(target)
 	if err != nil {
 		return err
 	}
 	res , err := http.Post(basePath+"/upstreams/"+service.UpstreamService+"/targets","application/json",bytes.NewBuffer(_body))
-	//body ,_ := ioutil.ReadAll(res.Body)
-	//fmt.Println(string(body))
 	defer res.Body.Close()
 	if err != nil {
 		return err
 	}
 	if res.StatusCode <= 400{
+		fmt.Println("OK")
 		return nil
 	}else{
 		return errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
@@ -56,6 +59,7 @@ func AddUpstreamTarget(service config.KongDesc , target UpstreamTarget) error {
 	}
 }
 func CreateService(serviceId string,service config.Services , serviceEndpointHost string , serviceEndpointPort int) error {
+	fmt.Print("\nCreating a new Service [",serviceId,"] .... ")
 	var _body []byte
 	var err error
 	if service.KongConf.Upstream == true{
@@ -80,13 +84,12 @@ func CreateService(serviceId string,service config.Services , serviceEndpointHos
 		}
 	}
 	res , err := http.Post(basePath+"/services/","application/json",bytes.NewBuffer(_body))
-	//body ,_ := ioutil.ReadAll(res.Body)
-	//fmt.Println(string(body))
 	defer res.Body.Close()
 	if err != nil {
 		return err
 	}
 	if res.StatusCode <= 400 || res.StatusCode == 409{
+		fmt.Println("OK")
 		return nil
 	}else{
 		return errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
@@ -94,6 +97,7 @@ func CreateService(serviceId string,service config.Services , serviceEndpointHos
 	}
 }
 func CreateNewRoute(serviceId string ,service config.Services) error{
+	fmt.Print("\nCreating a new Route [",service.KongConf.Route,"] [",serviceId,"] .... ")
 	_body , err := json.Marshal(Route{
 		Paths: []string{service.KongConf.Route},
 		Name: serviceId+"-Route",
@@ -107,9 +111,74 @@ func CreateNewRoute(serviceId string ,service config.Services) error{
 		return err
 	}
 	if res.StatusCode <= 400 || res.StatusCode == 409{
+		fmt.Println("OK")
 		return nil
 	}else{
 		return errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
+
+	}
+}
+func GetUpstreams() (UpstreamResp,error) {
+	res , err := http.Get(basePath+"/upstreams/")
+	body ,_ := ioutil.ReadAll(res.Body)
+	upstreams := UpstreamResp{}
+	json.Unmarshal(body,&upstreams)
+	defer res.Body.Close()
+	if err != nil {
+		return upstreams , err
+	}
+	if res.StatusCode <= 400{
+		return upstreams , nil
+	}else{
+		return upstreams , errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
+
+	}
+}
+func GetServices() (ServiceResp,error){
+	res , err := http.Get(basePath+"/services/")
+	body ,_ := ioutil.ReadAll(res.Body)
+	services := ServiceResp{}
+	json.Unmarshal(body,&services)
+	defer res.Body.Close()
+	if err != nil {
+		return services , err
+	}
+	if res.StatusCode <= 400{
+		return services , nil
+	}else{
+		return services , errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
+
+	}
+}
+func GetRoutes(serviceId string) (RouteResp,error){
+	res , err := http.Get(basePath+"/services/"+serviceId+"/routes")
+	body ,_ := ioutil.ReadAll(res.Body)
+	routes := RouteResp{}
+	json.Unmarshal(body,&routes)
+	defer res.Body.Close()
+	if err != nil {
+		return routes , err
+	}
+	if res.StatusCode <= 400{
+		return routes, nil
+	}else{
+		return routes , errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
+
+	}
+}
+func GetTargets(upstreamId string)(TargetResp,error){
+	res , err := http.Get(basePath+"/upstreams/"+upstreamId+"/targets")
+	body ,_ := ioutil.ReadAll(res.Body)
+	targets := TargetResp{}
+	json.Unmarshal(body,&targets)
+	defer res.Body.Close()
+	if err != nil {
+		return targets , err
+	}
+	if res.StatusCode <= 400{
+		return targets, nil
+	}else{
+		return targets , errors.New("Request Failed with a status of " + fmt.Sprintf("%d",res.StatusCode))
 
 	}
 }
--- kong/types.go
+++ kong/types.go
@@ -23,10 +23,37 @@ type Route struct {
 }
 
 type UpstreamResp struct {
-	next string `json:"next"`
-	data []struct{
+	Next string `json:"next"`
+	Data []struct{
 		Id  string `json:"id"`
 		Name string `json:"name"`
 		HashOn string `json:"hash_on"`
 	}  `mapstring:"data"`
+}
+type ServiceResp struct {
+	Next string `json:"next"`
+	Data []struct{
+		Id  string `json:"id"`
+		Host string `json:"host"`
+		Protocol string `json:"protocol"`
+		Port int `json:"port"`
+		Name string `json:"name"`
+		Path string `json:"path"`
+	}  `mapstring:"data"`
+}
+type RouteResp struct{
+	Next string `json:"next"`
+	Data []struct{
+		Id  string `json:"id"`
+		Paths []string `json:"paths"`
+		Name string `json:"name"`
+	}  `mapstring:"data"`
+}
+type TargetResp struct {
+	Next string `json:"next"`
+	Data []struct {
+		Id     string `json:"id"`
+		Weight int    `json:"weight"`
+		Target string `json:"target"`
+	} `mapstring:"data"`
 }
